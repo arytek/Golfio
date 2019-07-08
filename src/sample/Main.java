043aa9961fdc0eaac9680ef91b76e289796faaba
Jonathan-Choi27 <jonathan.choi27@gmail.com>
2019-07-08 16:34:30 +1000

    Basic configuration.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("stage1.fxml"));
+        primaryStage.setTitle("Golfio");
+        primaryStage.setScene(new Scene(root, 1000, 500));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
